647d67a713cb589c860ceab888f6df3b2ff8dc30
TODARO Anthony <atodaro77@gmail.com>
2026-01-26 23:04:04 +0100

    band

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import HeroSection from "@/components/home/HeroSection";
 import IntroSection from "@/components/home/IntroSection";
 import TeamPreview from "@/components/home/TeamPreview";
 import CTASection from "@/components/home/CTASection";
-import FlashNewsTicker from "@/components/home/FlashNewsTicker";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
-      <FlashNewsTicker />
       <IntroSection />
       <TeamPreview />
       <CTASection />
