a88bf6ee38572483490dfc1829e5802f412a2b74
TODARO Anthony <atodaro77@gmail.com>
2026-08-09 06:01:00 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
@@ -75,7 +76,11 @@ export default function RootLayout({
             <ChatBot />
           </ErrorBoundary>
         </div>
-		
+		<Script
+  data-goatcounter="https://ehpadcrecy.goatcounter.com/count"
+  async
+  src="https://gc.zgo.at/count.js"
+/>
       </body>
     </html>
   );
